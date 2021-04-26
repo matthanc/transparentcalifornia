f7079355dadf30bd6edab24a0eab2e2d02919c0b
Matt Churchill <matthanc@gmail.com>
2021-04-26 11:40:06 -0700

    Add files via upload

--- transparentcalifornia.R
+++ transparentcalifornia.R
@@ -1,53 +1,71 @@
 library(rvest)
 library(tidyverse)
+library(lubridate)
+library(ggmap)
+library(gmt)
 
+tc_scrape1 <- read_html("https://transparentcalifornia.com/agencies/salaries/")
+tc_scrape2 <- read_html("https://transparentcalifornia.com/agencies/salaries/special-districts/")
 
-tc_scrape <- read_html("https://transparentcalifornia.com/agencies/salaries/")
+tc_tables1 <- tc_scrape1 %>% html_nodes("table") %>% html_table(fill = TRUE)
+tc_attrs1 <- tc_scrape1 %>% html_nodes("table td a") %>% html_attrs()
+tc_tables2 <- tc_scrape2 %>% html_nodes("table") %>% html_table(fill = TRUE)
+tc_attrs2 <- tc_scrape2 %>% html_nodes("table td a") %>% html_attrs()
 
-tc_tables <- tc_scrape %>% html_table(fill = TRUE)
+tc_orgs1 <- tc_tables1 %>% reduce(rbind)
+tc_url1 <- tc_attrs1 %>% reduce(rbind)
+tc_orgs2 <- tc_tables2 %>% reduce(rbind)
+tc_url2 <- tc_attrs2 %>% reduce(rbind)
 
-tc_orgs <- bind_rows(tc_tables[[1]], tc_tables[[2]], tc_tables[[3]], tc_tables[[4]], tc_tables[[5]])
+tc_url1 <- substr(tc_url1,16,100)
+tc_url2 <- substr(tc_url2,16,100)
+
+tc_url1 <- as.tibble(tc_url1)
+tc_url2 <- as.tibble(tc_url2)
+
+tc_url1 <- unique(tc_url1)
+tc_url2 <- unique(tc_url2)
+
+tc_url1 <- tc_url1 %>% mutate(href = gsub("/", "", tc_url1$href))
+tc_url2 <- tc_url2 %>% mutate(href = gsub("/", "", tc_url2$href))
+
+tc_orgs1 <- bind_cols(tc_orgs1, tc_url1)
+tc_orgs2 <- bind_cols(tc_orgs2, tc_url2)
+
+tc_orgs <- bind_rows(tc_orgs1, tc_orgs2)
 
 tc_orgs$X2 <- substr(tc_orgs$X2,1,4)
+
 tc_orgs <- tc_orgs %>% filter(str_detect(X2,"20"))
-tc_orgs <- tc_orgs %>% mutate(url = tolower(tc_orgs$X1))
-tc_orgs <- tc_orgs %>% mutate(url = gsub("college of the", "college", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = gsub("college of", "college", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = gsub("the state bar of california", "state bar california", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = gsub("coast community college district", "coast district", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = gsub("foothill deanza community college district", "foothill deanza district", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = gsub("los angeles community college district", "los angeles district", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = gsub(" ", "-", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = sub("\\.", "", tc_orgs$url))
-tc_orgs <- tc_orgs %>% mutate(url = paste0("https://transcal.s3.amazonaws.com/public/export/",tc_orgs$url,"-",tc_orgs$X2,".csv"))
-tc_orgs <- tc_orgs %>% rename(org = X1, year = X2)
+tc_orgs <- tc_orgs %>% filter(str_detect(X2, as.character(year(now())-1)) |str_detect(X2, as.character(year(now())-2)))
+tc_orgs <- tc_orgs %>% mutate(href = paste0("https://transcal.s3.amazonaws.com/public/export/",tc_orgs$href,"-",tc_orgs$X2,".csv"))
+tc_orgs <- tc_orgs %>% rename(org = X1, year = X2, url = href)
 
-df <- map_df(tc_orgs$url, read.csv)
+tc_orgs <- inner_join(tc_orgs, read_csv("https://raw.githubusercontent.com/matthanc/transparentcalifornia/main/coords.csv"), by = "org")
 
-df <- map_dfr(tc_orgs$url, ~ read_csv(.x) %>%
+tcfilter <- function(organization, filterdist) {
+  lon <- as.numeric(filter(tc_orgs, org == organization) %>% select(coordinates.lon))
+  lat <- as.numeric(filter(tc_orgs, org == organization) %>% select(coordinates.lat))
+  tc_orgs_filtered <- tc_orgs %>% mutate(distance = geodist(lat, lon, tc_orgs$coordinates.lat, tc_orgs$coordinates.lon, units = "km") * 0.62137119) %>%
+    filter(distance <= filterdist)
+  return(tc_orgs_filtered)
+}
+
+tc_orgs_filtered <- tcfilter("San Francisco", 75)
+
+df_filtered <- map_dfr(tc_orgs_filtered$url, ~ read_csv(.x) %>%
                 mutate(across(everything(), as.character)))
+
+test <- tail(tc_orgs, n=31)
+testdf <- map_df(test$url, read.csv)
+
+testdf <- map_dfr(test$url, ~ read_csv(.x) %>%
+                mutate(across(everything(), as.character)))
+
 
 #next steps, make the above workflow 1 of larger function
 #need to prepare list of email suffixes - start by filtering only bay area cities and counties, or within 75 miles or so
 
-tc_orgs <- full_join(tc_orgs, read_csv("https://raw.githubusercontent.com/matthanc/transparentcalifornia/main/coords.csv"), by = "org")
-
 #next write a function that finds and filters distance from specific city on list of tc_orgs
 #inputs cityname and distance from in miles or whatever possible
 
-library(gmt)
-
-geodist(-117,34.6,-119,34.2, units = "km")
-
-filter(tc_orgs, org == "San Francisco") %>% select(coordinates.lon)
-filter(tc_orgs, org == "San Francisco") %>% select(coordinates.lat)
-
-tcfilter <- function(organization, distance) {
-  lon <- filter(tc_orgs, org == organization) %>% select(coordinates.lon)
-  lat <- filter(tc_orgs, org == organization) %>% select(coordinates.lat)
-  tc_orgs <- mutate(distance = geodist(lat, lon, tc_orgs$coordinates.lat, tc_orgs$coordinates.lon, units = "km") * 0.62137119)
-  tc_orgs_filtered <- filter(tc_orgs, distance <= distance)
-  return(tc_orgs_filtered)
-}
-
-tc_orgs_filtered <- tcfilter("san francisco", 75)
